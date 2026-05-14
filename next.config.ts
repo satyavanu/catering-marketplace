d9e62612357a4eecf798a967dc716bc9c7eb7c4e
Satyanarayana Vanukuru <satyavanu@gmail.com>
2026-05-14 20:23:10 +0200

    code revert

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['example.com'],
   },
-  env:{
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    AUTH_URL: process.env.AUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
-  },
   outputFileTracingIncludes: {
     '/*': ['./.env.production'],
   },
